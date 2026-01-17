81c390d543afeb7c1d597df4dba058ef60a20be6
DESKTOP-DSEJ5LA\tomec <tomeczq2@gmail.com>
2026-01-17 22:52:11 +0100

    postgres connection and migrations

--- app/src/main.py
+++ app/src/main.py
@@ -1,3 +1,5 @@
+import psycopg2
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,3 +10,21 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+def get_db():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+
+@app.route("/users")
+def users():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM users;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(rows)
